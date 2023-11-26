3b56d144d8c22d32bf3840ee699d3a15f0689c96
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2023-11-26 14:10:27 +0100

    Added API endpoints worked in class

--- index.js
+++ index.js
@@ -26,3 +26,43 @@ expressAPP.listen(PORT, () => {
 expressAPP.get("/info", (request, response) => {
   response.json({ version: "0.0.1" });
 });
+
+// Definimos una estructura de datos
+// (temporal hasta incorporar una base de datos)
+let coches = [
+  { marca: "Renault", modelo: "Clio" },
+  { marca: "Nissan", modelo: "Skyline R34" },
+];
+
+// Lista todos los coches
+app.get("/coches", (request, response) => {
+  response.json(coches);
+});
+
+// Añadir un nuevo coche
+app.post("/coches", (request, response) => {
+  coches.push(request.body);
+  response.json({ message: "ok" });
+});
+
+// Obtener un solo coche
+app.get("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  const result = coches[id];
+  response.json({ result });
+});
+
+// Actualizar un solo coche
+app.put("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  coches[id] = request.body;
+  response.json({ message: "ok" });
+});
+
+// Borrar un elemento del array
+app.delete("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  coches = coches.filter((item) => coches.indexOf(item) !== id);
+
+  response.json({ message: "ok" });
+});
